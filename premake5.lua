3e58ac586e967a1c3b293f73312582eefa6a9578
Lorenzo Fortina <lorenzo.fortina.95@gmail.com>
2020-07-06 16:46:09 +0200

    Create premake5.lua

---
+++ premake5.lua
@@ -0,0 +1,62 @@
+project "assimp"
+	kind "StaticLib"
+	language "C++"
+	cppdialect "C++14"
+	staticruntime "On"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"code/**.cpp",
+		"code/**.h",
+		"contrib/irrXML/*.cpp",
+		"contrib/irrXML/*.h",
+		"contrib/unzip/*.c",
+		"contrib/unzip/*.h",
+		"contrib/zlib/*.c",
+		"contrib/openddlparser/code/*.cpp",
+		"contrib/poly2tri/poly2tri/**.cc",
+		"contrib/clipper/*.cpp",
+	}
+
+	includedirs 
+	{
+		"",
+		"code/",
+		"include/",
+		"contrib/irrXML/",
+		"contrib/rapidjson/include/",
+		"contrib/openddlparser/include/",
+		"contrib/unzip/",
+		"contrib/zlib/",
+	}
+
+	defines 
+	{
+		"_CRT_SECURE_NO_WARNINGS",
+		"ASSIMP_BUILD_NO_C4D_IMPORTER",
+		"OPENDDL_STATIC_LIBARY",
+	}
+
+	removefiles 
+	{
+		"code/Importer/IFC/IFCReaderGen_4.*",
+	}
+
+	buildoptions  
+	{
+		"/bigobj"
+	}
+
+	filter "system:windows"
+		systemversion "latest"
+
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"	-- debug version --
+
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"	-- release version --
